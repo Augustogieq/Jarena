96f027cf25a0ea7dbb9ce42650130f75c4b01e01
Augusto Giequelin <gieq@MacBook-Pro-de-Augusto-2.local>
2022-01-20 11:56:00 -0300

    'Comecando a utilizar as coordenadas para movimentacao do agente no mapa'

--- src/br/uffs/cc/jarena/AgenteAugusto.java
+++ src/br/uffs/cc/jarena/AgenteAugusto.java
@@ -10,19 +10,51 @@ package br.uffs.cc.jarena;
 
 public class AgenteAugusto extends Agente
 {
+	private double rand,cont;
+    private Boolean centro = false;
+    //starta as variaveis de controle de localizacao
+
+	
 	public AgenteAugusto(Integer x, Integer y, Integer energia) {
 		super(x, y, energia);
 		setDirecao(geraDirecaoAleatoria());
 	}
 	
 	public void pensa() {
+		//removida a estrategia de se mover aleatoriamente e startado a operacao de setar a movimentação
+		if (this.cont < 0.08) {
 
-		if(!podeMoverPara(getDirecao())) {
-			setDirecao(geraDirecaoAleatoria());
+            if(!this.centro) {
+
+                rand = Math.random();
+                if(getX() < 450){
+                    if(rand > 0.5) setDirecao(DIREITA);
+                    else{
+
+                        if(getY() < 350) setDirecao(BAIXO);
+                        else setDirecao(CIMA);
+
+                    }
+                } else {
+                    if(rand > 0.5) setDirecao(ESQUERDA);
+                    else{
+
+                        if(getY() < 350) setDirecao(BAIXO);
+                        else setDirecao(CIMA);
+
+                    }
+                }    
+            }
+            else {
+                setDirecao(geraDirecaoAleatoria());
+            }
+        }
+
+
 		}
 		
-		// if(podeDividir() && getEnergia() >= 800) {
-	//		divide();
+	//if(podeDividir() && getEnergia() >= 800) {
+	//	divide(); cancelamos a divisao por enquanto
 		}
 	}
 	
@@ -43,6 +75,9 @@ public class AgenteAugusto extends Agente
 		// Invocado sempre que um agente aliado próximo envia uma mensagem.
 	}
 	
+	public void para(){
+		
+	}
 	public String getEquipe() {
 		// Definimos que o nome da equipe do agente é "Augusto".
 		return "Augusto";
